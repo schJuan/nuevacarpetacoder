4c96a8e3d0cb93c01d789272eea00fd6be4a91ac
schlotthauerjuan <juan_41629456gmail.com>
2023-09-25 20:53:30 -0300

    algunas pruebas en JS

---
+++ app.js
@@ -0,0 +1,42 @@
+//let nombre = "CoderHause"
+//let edad = 25
+//let primerNombre = "Coder"
+//let apellido= "house"
+//let numero1 = 30
+//let numero2= 30
+
+//console.log(primerNombre)
+//console.log(apellido)
+
+//console.log (numero1 + numero2)
+//alert(primerNombre + apellido)
+//let nombre = prompt("ingrese su nombre") 
+/*if (nombre > Jschlott ) {
+    console.log("ingreso exitoso")
+}else{
+    console.log("Vayase a la mierda")
+}*/
+
+/*let edad = prompt("ingresa tu edad")
+if(edad >= 18){
+    console.log("ingeso exitoso")
+}else{
+    console.log("debes ser mayor de edad para ingresar")
+}*/
+/*let calificacion = prompt("cual fue tu calificacion")
+
+if (calificacion >=8) {
+    console.log = alert("calificacion sobresaliente")
+} else if (calificacion >=6) {
+    console.log = alert("aprobado")
+} else{
+    console.log = alert("debes recursar")
+} */
+let nombreUsuario = prompt ("Nombre de usuario")
+let apellidoUsuario = prompt("Apellido de usuario")
+
+if((nombreUsuario !="") && (apellidoUsuario !="")){
+    console.log("Nombre: "+ nombreUsuario+ " Apellido: " + apellidoUsuario)
+}else{
+    console.log = alert("Todos los campos son obligatorios")
+}
