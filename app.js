98a38bd3c05d028beea672e9f17f971d7037176b
schlotthauerjuan <juan_41629456gmail.com>
2023-10-02 10:44:36 -0300

    PrimeraPreEntregaJS

--- app.js
+++ app.js
@@ -1,42 +1,44 @@
-//let nombre = "CoderHause"
-//let edad = 25
-//let primerNombre = "Coder"
-//let apellido= "house"
-//let numero1 = 30
-//let numero2= 30
-
-//console.log(primerNombre)
-//console.log(apellido)
-
-//console.log (numero1 + numero2)
-//alert(primerNombre + apellido)
-//let nombre = prompt("ingrese su nombre") 
-/*if (nombre > Jschlott ) {
-    console.log("ingreso exitoso")
-}else{
-    console.log("Vayase a la mierda")
-}*/
-
-/*let edad = prompt("ingresa tu edad")
-if(edad >= 18){
-    console.log("ingeso exitoso")
-}else{
-    console.log("debes ser mayor de edad para ingresar")
-}*/
-/*let calificacion = prompt("cual fue tu calificacion")
-
-if (calificacion >=8) {
-    console.log = alert("calificacion sobresaliente")
-} else if (calificacion >=6) {
-    console.log = alert("aprobado")
-} else{
-    console.log = alert("debes recursar")
-} */
-let nombreUsuario = prompt ("Nombre de usuario")
-let apellidoUsuario = prompt("Apellido de usuario")
-
-if((nombreUsuario !="") && (apellidoUsuario !="")){
-    console.log("Nombre: "+ nombreUsuario+ " Apellido: " + apellidoUsuario)
-}else{
-    console.log = alert("Todos los campos son obligatorios")
+let nombreUsuario = prompt ("Ingrese su nombre")
+let apellido = prompt ("Ingrese su apellido")
+let entrada = prompt (`Que desa pedir ${nombreUsuario} ${apellido}
+1 Burguer clasica $1000
+2 Triple queso $2000
+3 Musk burguer $3000`)
+switch(entrada){
+    case "1":
+        alert("Usted pidió Burguer clasica")
+        break
+    case "2":
+        alert("Usted pidió Triple queso")
+        break
+    case "3":
+        alert("Usted pidió Musk Burguer")
+        break
+    default:
+        alert("No entendi el mensaje, intente de nuevo")
+        break
+}
+let papas = prompt(`¿Desea agregar papas por $600? responda "Si" o "No"`)
+switch (papas) {
+    case "Si":
+        alert("Su hamburguesa va con papas")
+        break
+    case "No":
+        alert("Su hamburguesa va huerfana (sin papas)")
+        break
+    default:
+        alert("No entendi el mensaje, intente de nuevo")
+        break
+}
+let pedido = (`${entrada} ${papas}`)
+switch (pedido) {
+    case "1 Si":
+        alert(`${nombreUsuario} su pedido es de $1600`)
+        break
+    case "2 SI":
+        alert(`${nombreUsuario} su pedido es de $2600`)
+        break
+    case "3 SI":
+        alert(`${nombreUsuario} su pedido es de $3600`)
+        break
 }
