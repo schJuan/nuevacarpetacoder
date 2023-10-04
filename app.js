47c256e864cba8e9fbea5b4552b220e859120be7
schlotthauerjuan <juan_41629456gmail.com>
2023-10-04 16:00:53 -0300

    clase objetos

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-let nombreUsuario = prompt ("Ingrese su nombre")
+/*let nombreUsuario = prompt ("Ingrese su nombre")
+
 let apellido = prompt ("Ingrese su apellido")
+
 let entrada = prompt (`Que desa pedir ${nombreUsuario} ${apellido}
 1 Burguer clasica $1000
 2 Triple queso $2000
 3 Musk burguer $3000`)
+
 switch(entrada){
     case "1":
         alert("Usted pidió Burguer clasica")
@@ -18,7 +21,9 @@ switch(entrada){
         alert("No entendi el mensaje, intente de nuevo")
         break
 }
+
 let papas = prompt(`¿Desea agregar papas por $600? responda "Si" o "No"`)
+
 switch (papas) {
     case "Si":
         alert("Su hamburguesa va con papas")
@@ -30,15 +35,48 @@ switch (papas) {
         alert("No entendi el mensaje, intente de nuevo")
         break
 }
+
 let pedido = (`${entrada} ${papas}`)
+
 switch (pedido) {
     case "1 Si":
         alert(`${nombreUsuario} su pedido es de $1600`)
         break
-    case "2 SI":
+    case "2 Si":
         alert(`${nombreUsuario} su pedido es de $2600`)
         break
-    case "3 SI":
+    case "3 Si":
         alert(`${nombreUsuario} su pedido es de $3600`)
         break
+}*/
+
+/*const producto = {
+    nombre: "Monitor de 24 pulgadas",
+    precio: 2000,
+    disponible: true,
+    
+}*/
+
+//console.log(producto.origen.pais)
+
+//const nombreProducto = producto.nombre
+
+//console.log(nombreProducto)
+//create
+//producto.imagen = "image.jpg"
+// update
+//producto.disponible = false
+// delete
+//delete producto.precio
+//console.log(producto)
+
+/*const { nombre, imagen, disponible} = producto
+console.log(nombre)*/
+
+const usuario = {
+    nombre: "Coder",
+    apellido: "house",
+    edad: 24,
+    email: "coder@house.com"
 }
+console.log(usuario)
